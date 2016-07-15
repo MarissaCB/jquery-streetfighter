e01289ddf6d839ea4504053c1998d631cbb2782d
MarissaCB <marissacody@gmail.com>
2016-07-14 21:51:32 -0400

    commit java streetfighter

--- js/app.js
+++ js/app.js
@@ -8,19 +8,42 @@ $(document).ready(function() {
 		$('.ryu-still').show();	
 	})
 	.mousedown(function() {
-		console.log('mousedown');
+		playHadouken();
 // play hadouken sound
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();	
-		$('.hadouken').show();	
-// animate hadouken to the right of the screen
+		$('.hadouken').show().animate(
+			{'left': '1100px'},
+			500,
+			function() {
+			  $(this).hide();
+			  $(this).css('left', '600px');
+			});
 	})
+// ryu goes back to his ready position	
 	.mouseup(function() {
-		console.log('mouseup');
-	// ryu goes back to his ready position	
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });	
-	
+	function playHadouken() {
+    	$('#hadouken-sound')[0].volume = 0.5;
+    	$('#hadouken-sound')[0].load();
+    	$('#hadouken-sound')[0].play();
+}
+/* play coolmusic sound	
+.keydown(function() {
+		playCoolmusic();
 
+		$('.ryu-still').hide();
+		$('.ryu-coolmusic').show();	
+					
+	})
+
+function playCoolmusic() {
+    	$('#hadouken-sound')[0].volume = 0.5;
+    	$('#hadouken-sound')[0].load();
+    	$('#hadouken-sound')[0].play();
+}
+
+play coolmusic sound*/
